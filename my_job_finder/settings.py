6a6596f7108ac9b49cb49b4f850f8c545d130f67
Omkar <omkar1jadhav0@gmail.com>
2025-07-18 09:37:20 +0530

    added requirements.txt

--- my_job_finder/settings.py
+++ my_job_finder/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    'django_bootstrap5',
     'users',
     'jobs',
     'django_ckeditor_5',
