3dd3491a9e14acbc73a0155a8ff1af65d4592f3f
Omkar <omkar1jadhav0@gmail.com>
2025-07-25 21:33:05 +0530

    Ready for production

--- my_job_finder/settings.py
+++ my_job_finder/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 AUTH_USER_MODEL = 'users.CustomUser'
 
 
