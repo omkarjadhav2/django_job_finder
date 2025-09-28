f5af0d1723cce57b2226a71b00f9e8e8c12c0a63
Omkar <omkar1jadhav0@gmail.com>
2025-09-28 14:04:15 +0530

    ready for production

--- my_job_finder/settings.py
+++ my_job_finder/settings.py
@@ -88,14 +88,19 @@ WSGI_APPLICATION = 'my_job_finder.wsgi.application'
 
 import dj_database_url
 
-
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        ssl_require=True  
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
